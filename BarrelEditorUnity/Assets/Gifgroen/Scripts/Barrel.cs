4d22f401c1724984fe676a2aa4d12c001a0bc02e
Karsten Westra <karstenwestra@gmail.com>
2020-09-30 15:48:34 +0200

    Added simple Unity project with simple Barrel script.

---
+++ BarrelEditorUnity/Assets/Gifgroen/Scripts/Barrel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Gifgroen
+{
+    public class Barrel : MonoBehaviour
+    {
+#pragma warning disable 0649
+        [SerializeField] private float radius = 1;
+
+        [SerializeField] private float damage = 10;
+
+        [SerializeField] private Color color = Color.red;
+#pragma warning restore 0649
+    }
+}
